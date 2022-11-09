54b881bfe69f249847017a0c4692b9d2c2c8cc3b
Tarun03022407 <taggarwal533@gmail.com>
2022-11-09 22:15:44 +0530

    day-1-navbar

---
+++ trendz-mart/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Navbar from './Navbar/Navbar';
+// import Women from './NavbarMiddle/Women';
+import NavbarMiddle from './NavbarMiddle/NavbarMiddle';
+
+function App() {
+  return (
+    <div className="App">
+   <Navbar />
+   <NavbarMiddle/>
+    </div>
+  );
+}
+
+export default App;
---
+++ trendz-mart/src/NavbarMiddle/Men.jsx
@@ -0,0 +1,21 @@
+import { Stack,Button } from "@chakra-ui/react"
+
+const Men=()=>{
+    return (
+        <>
+        <div>
+        <Stack  >
+   <Button  display= "inline-block"
+maxWidth=" 108px"
+whiteSpace= "nowrap"
+overflow= "hidden"_hover={{backgroundColor:"#fffe94"}} marginLeft={"30px"} fontWeight={"bold"}   textOverflow= "ellipsis"  fontSize=".87rem" background="white" width={"70px"} size={2} mt={3} > 
+Men
+
+</Button>
+
+ </Stack  >
+        </div>
+        </>
+    )
+}
+export default Men
